ffc9f225836c3afcf58476957f7888eae5ce7b63
fontoful <fonto.serrano@gmail.com>
2023-08-21 16:23:02 -0600

    debug settings

--- myudemyproject/settings.py
+++ myudemyproject/settings.py
@@ -24,9 +24,10 @@ SECRET_KEY = 'django-insecure-g$zm&l&oknc(ws6!me_%)7p&7uekpwu0mctuq2tre7^x#&!!*g
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
 
+# DEBUG = False
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
